5e622e8a0778905293a4f78018e61c1ea24606fa
Jane Nguyen <janele@gmail.com>
2014-03-29 15:31:22 -0700

    write results to file

--- juicy.rb
+++ juicy.rb
@@ -4,23 +4,24 @@ require 'pry'
 require 'json'
 require 'geocoder'
 
-def get_coordinates(street, city)
-    puts "got here2"
-    puts "inside get_coordinates #{street}" 
-    puts "inside get_coordinates #{city}"  
+#put the business name, coordinates, image url into a file
+def get_address_coordinates(street, city)
+    # puts "got in get_location_coordinates"
+    # puts "inside get_coordinates #{street}"
+    # puts "inside get_coordinates #{city}"
 
     coordinate_results = Geocoder.search("#{street}, #{city}")
-    puts "the results are #{coordinate_results}"
-    #=> {"lat"=>37.7883385, "lng"=>-122.3991915}
-    coordinate_results_lat = coordinate_results[0].data["geometry"]["location"]["lat"]
-    coordinate_results_lng = coordinate_results[0].data["geometry"]["location"]["lat"]
-    puts "final coordinates are #{coordinate_results_lat}, #{coordinate_results_lng}"
-    binding.pry 
+    return_result = coordinate_results[0].data["geometry"]["location"]
+    # puts "the results are #{coordinate_results}"
+    # coordinate_results_lat = coordinate_results[0].data["geometry"]["location"]["lat"]
+    # coordinate_results_lng = coordinate_results[0].data["geometry"]["location"]["lat"]
+    # puts "final coordinates are #{coordinate_results_lat}, #{coordinate_results_lng}"
+    #binding.pry
 end
 
 
 def jcy
-
+	# Setup
 	consumer_key = 'YdXRvSA-a3sh0Fz8UNH0sQ'
 	consumer_secret = 'KWwFyUJwTbAu6SYgT4VAIAGU9oQ'
 	token = '5IUdhH-GTVblaa0BhOFrua6m-BO4M7ai'
@@ -31,26 +32,32 @@ def jcy
 	consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
 	access_token = OAuth::AccessToken.new(consumer, token, token_secret)
 
+	# Set the file for writing
+	file = File.open("restaurants_data.txt", "w")
+
+	# Set the location
 	#path = "/v2/search?term=restaurants&location=new%20york"
 	path = "/v2/search?term=food&ll=37.483258,-122.149623"
 
-
+	# Get information on a list restaurants based on the location set above
 	body = access_token.get(path).body
 	h = JSON.parse(body)
 	h["businesses"].each do | business |
 		business_id = business["id"]
-		puts business["id"]
-		puts business["name"]
-		puts business["image_url"]
-		path = "/v2/business/roys-restaurant-san-francisco"
+		business_name = business["name"]
+		business_image = business["image_url"]
+
+		path = "/v2/business/#{business_id}"
 		body = access_token.get(path).body
 		address = JSON.parse(body)
-		get_coordinates(address["location"]["address"].first, address["location"]["city"])
-		puts "got here"
-		binding.pry
+		business_coordinates = get_address_coordinates(address["location"]["address"].first, address["location"]["city"])
+		 file.write("name: #{business_name}, coordinates: #{business_coordinates}, image_url: #{business_image}\n")
+		# puts "name: #{business_name}, coordinates: #{business_coordinates}, image_url: #{business_image}"
+		# puts "got here"
+		# binding.pry
 	end
-	
-	p body
+	file.close
+	# p body
 end
 
 jcy
